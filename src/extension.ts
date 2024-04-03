02af5e8acfce796074884a5f4f74ff4cfcac1a99
SangYuo <sangxilam@gmail.com>
2024-04-03 10:35:24 +0700

    feat(class): add sn text

--- src/extension.ts
+++ src/extension.ts
@@ -20,32 +20,27 @@ export async function activate(context: vscode.ExtensionContext) {
           new vscode.Range(new vscode.Position(position.line, 0), position)
         );
         let completionItems: any[] = [];
-        if (lineUntilPos && lineUntilPos.length) {
-          if (lineUntilPos?.includes("className")) {
-            for (const key in keyStyles) {
-              const item: {
-                prefix: string;
-                description: string;
-                color?: string;
-              } = keyStyles[key];
-              const completionItem = new vscode.CompletionItem(
-                item.prefix,
-                item.color
-                  ? vscode.CompletionItemKind.Color
-                  : vscode.CompletionItemKind.Enum
+        if (lineUntilPos?.includes("className")) {
+          for (const key in keyStyles) {
+            const item: {
+              prefix: string;
+              description: string;
+              color?: string;
+            } = keyStyles[key];
+            const completionItem = new vscode.CompletionItem(
+              item.prefix,
+              item.color
+                ? vscode.CompletionItemKind.Color
+                : vscode.CompletionItemKind.Enum
+            );
+            if (item.color) {
+              completionItem.documentation = new vscode.MarkdownString(
+                `Color: ${item.color}`
               );
-              if (item.color) {
-                completionItem.documentation = new vscode.MarkdownString(
-                  `Color: ${item.color}`
-                );
-              }
-              completionItem.detail = item.description;
-              completionItems.push(completionItem);
             }
+            completionItem.detail = item.description;
+            completionItems.push(completionItem);
           }
-        }
-        if (!completionItems.length) {
-          return undefined;
         }
         return [...completionItems];
       },
